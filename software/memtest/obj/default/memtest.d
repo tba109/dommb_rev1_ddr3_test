35e4d3c9181cc7898c6df0f054b0ff314d17e91b
Tyler Anderson <tba109@psu.edu>
2017-03-22 17:07:18 -0400

    V2.1: Now added altera's memtest to the software, which is passing the test

---
+++ software/memtest/obj/default/memtest.d
@@ -0,0 +1,40 @@
+obj/default/memtest.o: memtest.c ../test1_bsp//HAL/inc/alt_types.h \
+ ../test1_bsp//HAL/inc/io.h ../test1_bsp//HAL/inc/alt_types.h \
+ ../test1_bsp/system.h ../test1_bsp/linker.h \
+ ../test1_bsp//HAL/inc/sys/alt_dma.h \
+ ../test1_bsp//HAL/inc/sys/alt_dma_dev.h \
+ ../test1_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../test1_bsp//HAL/inc/sys/alt_llist.h \
+ ../test1_bsp//HAL/inc/sys/alt_flash.h \
+ ../test1_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../test1_bsp//HAL/inc/sys/alt_flash_dev.h \
+ ../test1_bsp//HAL/inc/sys/alt_cache.h \
+ ../test1_bsp//HAL/inc/sys/alt_flash_dev.h
+
+../test1_bsp//HAL/inc/alt_types.h:
+
+../test1_bsp//HAL/inc/io.h:
+
+../test1_bsp//HAL/inc/alt_types.h:
+
+../test1_bsp/system.h:
+
+../test1_bsp/linker.h:
+
+../test1_bsp//HAL/inc/sys/alt_dma.h:
+
+../test1_bsp//HAL/inc/sys/alt_dma_dev.h:
+
+../test1_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../test1_bsp//HAL/inc/sys/alt_llist.h:
+
+../test1_bsp//HAL/inc/sys/alt_flash.h:
+
+../test1_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../test1_bsp//HAL/inc/sys/alt_flash_dev.h:
+
+../test1_bsp//HAL/inc/sys/alt_cache.h:
+
+../test1_bsp//HAL/inc/sys/alt_flash_dev.h:
